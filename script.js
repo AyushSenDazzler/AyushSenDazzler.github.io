fbd9660c2c2ac2f5836493040e87214d141ecfc7
AYUSH SEN <85610426+AyushSenDazzler@users.noreply.github.com>
2023-06-06 14:23:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,62 @@
 const postForm = document.getElementById('post-form');
 const postsSection = document.getElementById('posts');
+const posts = [];
+
+// Load the Google Sign-In API
+function initializeGoogleAuth() {
+  gapi.load('auth2', () => {
+    gapi.auth2.init({
+      client_id: 'YOUR_CLIENT_ID',
+    });
+  });
+}
+
+// Sign in the user
+function signIn() {
+  gapi.auth2.getAuthInstance().signIn().then(() => {
+    console.log('User signed in.');
+  });
+}
+
+// Sign out the user
+function signOut() {
+  gapi.auth2.getAuthInstance().signOut().then(() => {
+    console.log('User signed out.');
+  });
+}
+
+// Check if the user is signed in
+function isUserSignedIn() {
+  return gapi.auth2.getAuthInstance().isSignedIn.get();
+}
+
+// Handle the sign-in status changes
+function onSignInStatusChange(isSignedIn) {
+  const signInButton = document.getElementById('sign-in-button');
+  const signOutButton = document.getElementById('sign-out-button');
+  const newPostSection = document.getElementById('new-post');
+
+  if (isSignedIn) {
+    signInButton.style.display = 'none';
+    signOutButton.style.display = 'block';
+    newPostSection.style.display = 'block';
+  } else {
+    signInButton.style.display = 'block';
+    signOutButton.style.display = 'none';
+    newPostSection.style.display = 'none';
+  }
+}
+
+// Add event listeners to the sign-in and sign-out buttons
+document.getElementById('sign-in-button').addEventListener('click', signIn);
+document.getElementById('sign-out-button').addEventListener('click', signOut);
+
+// Check the sign-in status on page load
+document.addEventListener('DOMContentLoaded', () => {
+  initializeGoogleAuth();
+  gapi.auth2.getAuthInstance().isSignedIn.listen(onSignInStatusChange);
+  onSignInStatusChange(isUserSignedIn());
+});
 
 postForm.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -11,16 +68,42 @@ postForm.addEventListener('submit', (event) => {
 });
 
 function createPost(title, content) {
-  const post = document.createElement('div');
-  post.className = 'post';
+  const post = {
+    title: title,
+    content: content
+  };
 
-  const postTitle = document.createElement('h3');
-  postTitle.textContent = title;
+  posts.push(post);
+  displayPosts();
+}
 
-  const postContent = document.createElement('p');
-  postContent.textContent = content;
+function displayPosts() {
+  postsSection.innerHTML = '';
 
-  post.appendChild(postTitle);
-  post.appendChild(postContent);
-  postsSection.appendChild(post);
+  posts.forEach((post, index) => {
+    const postElement = document.createElement('div');
+    postElement.className = 'post';
+
+    const postTitle = document.createElement('h3');
+    postTitle.textContent = post.title;
+
+    const postContent = document.createElement('p');
+    postContent.textContent = post.content;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', () => {
+      deletePost(index);
+    });
+
+    postElement.appendChild(postTitle);
+    postElement.appendChild(postContent);
+    postElement.appendChild(deleteButton);
+    postsSection.appendChild(postElement);
+  });
+}
+
+function deletePost(index) {
+  posts.splice(index, 1);
+  displayPosts();
 }
